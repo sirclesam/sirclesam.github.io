1a4be8302ba2821691422b3b5265177b993e18f8
Sam Selfridge <Sam.selfridge@gmail.com>
2021-03-24 21:53:11 -0700

    let the spidering begin

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  const images = importAll(require.context("../public/imgs", false, /\.(png|jpe?g|svg)$/));
+  console.log("images: ", images);
+
+  const rand = Math.floor(images.length * Math.random());
+  console.log("rand: ", rand);
+
+  const dickSource = images[rand].default;
+  console.log("dickSource: ", dickSource);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dickSource} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the Future home of <code>DickSpiers.com</code>
+        </p>
+        <p>Reload to see a new dick spider</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
