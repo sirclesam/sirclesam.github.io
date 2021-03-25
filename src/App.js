1d2a4c491c411654809fbf8a0fb6954ac3370b32
Sam Selfridge <Sam.selfridge@gmail.com>
2021-03-25 06:46:24 -0700

    Lets go with Quotes...cuz why not

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import "./App.css";
+// import { quotes } from "./quotes";
 
 function App() {
   function importAll(r) {
     return r.keys().map(r);
   }
-  function shuffle(array) {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
+  const quotes = [
+    "Fear is temporary and useless, glory is forever",
+    "The hills are alive with fucks I give on select topics.",
+    "Punch a velociraptor in its dick",
+    "You are the protagonist",
+    "After these messages, we'll be right back",
+    "Go deep throat a cactus.",
+    "Anything is a dildo if you're brave enough",
+    "What kind of fuckery is this?",
+    "If you don't like something it's okay to shut the fuck up about it and find something you do like.",
+    "Life is like a sewer… what you get out of it depends on what you put into it.",
+    "Bad decisions make good stories.",
+    "Well-behaved women seldom make history.",
+    "I didn’t fail the test. I just found 100 ways to do it wrong.",
+    "I may not have gone where I intended to go, but I think I have ended up where I needed to be.",
+    "Don't Panic.",
+    "If there's anything more important than my ego around, I want it caught and shot now.",
+    "I never could get the hang of Thursdays.",
+  ];
 
   const images = importAll(require.context("../public/imgs", false, /\.(png|jpe?g|svg)$/));
 
-  const randomImages = shuffle(images);
+  // const randomImages = shuffle(images);
+
+  const randomSrc = images[Math.floor(Math.random() * images.length)].default;
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
   return (
     <div className="App">
@@ -35,14 +40,13 @@ function App() {
           Welcome to the Future home of <code>DickSpiders.com</code>
         </p>
         <div className="spiders">
-          {randomImages.map((image) => (
-            <img
-              src={image.default}
-              className={Math.round(Math.random()) === 1 ? "App-logo" : "App-logo2"}
-              alt="logo"
-            />
-          ))}
+          <img
+            src={randomSrc}
+            className={Math.round(Math.random()) === 1 ? "App-logo" : "App-logo2"}
+            alt="logo"
+          />
         </div>
+        <h1 style={{ maxWidth: "80%", margin: "auto" }}>{randomQuote}</h1>
       </header>
     </div>
   );
---
+++ src/quotes.js
@@ -0,0 +1,9 @@
+export const quotes = [
+  "Fear is temporary and useless, glory is forever",
+  "The hills are alive with fucks I give on select topics.",
+  "Punch a velociraptor in its dick",
+  "You are the protagonist",
+  "After these messages, we'll be right back",
+  "Go deep throat a cactus.",
+  "Anything is a dildo if you're brave enough",
+];
